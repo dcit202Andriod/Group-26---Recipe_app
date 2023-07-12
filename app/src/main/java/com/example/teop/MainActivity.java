b6538ab4cb3664e51e3210c3ba43ebb7ee94fc06
YondaimeKirin <135043550+YondaimeKirin@users.noreply.github.com>
2023-07-12 14:00:07 +0000

    webview client neeeded

--- app/src/main/java/com/example/teop/MainActivity.java
+++ app/src/main/java/com/example/teop/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     private WebView mywebView;
