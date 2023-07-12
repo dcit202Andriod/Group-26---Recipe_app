bee71dd9a204e3769778e92abef789decf8b0c90
YondaimeKirin <135043550+YondaimeKirin@users.noreply.github.com>
2023-07-12 13:59:41 +0000

    Update MainActivity.java

--- app/src/main/java/com/example/teop/MainActivity.java
+++ app/src/main/java/com/example/teop/MainActivity.java
@@ -6,7 +6,6 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     private WebView mywebView;
